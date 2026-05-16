65320ab1de7bd3ceb187328294fca34f7c54af60
Ek3ot1k <aminguseynov90@gmail.com>
2026-05-16 22:44:21 +0300

    исправил http эндпоинт для регистрации 2

--- src/main/java/ru/amin/Rest/controllers/AuthController.java
+++ src/main/java/ru/amin/Rest/controllers/AuthController.java
@@ -23,7 +23,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("api/v1/auth")
 public class AuthController {
 
     private final ModelMapper modelMapper;
